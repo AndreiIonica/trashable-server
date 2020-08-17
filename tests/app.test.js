5915cc09671a10eb97d08a24f9d7464944c4f4ef
AndreiIonica <andreiionica42@gmail.com>
2020-08-17 18:38:41 +0300

    test setup

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,12 @@
 // Tests for the app
 // TODO: Dragos + set-up test db
 const supertest = require('supertest');
+const db = require('../src/db');
 
 const app = require('../src/app');
 
 // TODO: modify this test to a better one
-describe('GET /', () => {
+describe('GET /', function () {
   it('should respond with a message', async () => {
     const response = await supertest(app)
       .get('/')
